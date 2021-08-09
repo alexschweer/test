e56b88b84fa1dfcdd2a7a6fe3784939a1ead74d9
aschweer <alexander.schweer@studmail.w-hs.de>
2021-08-09 16:11:54 +0200

    Test

--- al_xen_part_a.sh
+++ al_xen_part_a.sh
@@ -1,7 +1,39 @@
+# Dependencies
+log_alert () {
+    echo "\e[1;33m[-] ALERT: $1\e[1;0m"
+}
+log_blue () {
+    echo "\e[1;34m$1\e[1;0m"
+}
+log_debug () {
+    echo "\e[1;36m[~] DEBUG: $1\e[1;0m"
+}
+log_error () {
+    echo "\e[1;31m[X] ERROR: $1\e[1;0m"
+}
+log_info () {
+    echo "\e[1;34m[*]\e[1;0m $1"
+}
+log_red () {
+    echo "\e[1;31m$1\e[1;0m"
+}
+log_success () {
+    echo "\e[1;32m[+] $1\e[1;0m"
+}
+log_yellow () {
+    echo "\e[1;33m$1\e[1;0m"
+}
+
+# *****************************************************************************************************************************************************
+# *****************************************************************************************************************************************************
+# *****************************************************************************************************************************************************
+
 # German keyboard layout
+log_blue "Loading German keyboard layout..."
 loadkeys de
 
 # Global variables
+log_blue "Initializing global variables..."
 DEV="/dev/sda" # Harddisk
 EFI="/dev/sda1" # EFI partition
 LUKS="/dev/sda2" # LUKS partition
@@ -9,9 +41,11 @@ UCODE="intel-ucode" # CPU microcode
 USER="alex" # Username
 
 # System clock
+log_blue "Enable network time synchronization..."
 timedatectl set-ntp true # Enable network time synchronization
 
 # Partitioning (GPT parititon table)
+log_blue "Partitioning the HDD/SSD with GPT partition layout..."
 sgdisk --zap-all $DEV # Wipe verything
 sgdisk --new=1:0:+512M $DEV # Create EFI partition
 sgdisk --new=2:0:0 $DEV # Create LUKS partition
@@ -20,33 +54,36 @@ sgdisk --change-name=1:efi-sp --change-name=2:luks $DEV # Label partitions
 sgdisk --print $DEV # Print partition table
 
 # LUKS 
+log_blue "Formatting the second partition as LUKS crypto partition..."
 cryptsetup luksFormat $LUKS --type luks1 -c twofish-xts-plain64 -h sha512 -s 512 # Format LUKS partition
 cryptsetup luksOpen $LUKS lukslvm # Open LUKS partition
 
 # LVM 
+log_blue "Setting up LVM..."
 pvcreate /dev/mapper/lukslvm # Create physical volume
 vgcreate luksvg /dev/mapper/lukslvm # Create volume group
 lvcreate -L 6144M luksvg -n swap # Create logical swap volume
 lvcreate -l 100%FREE luksvg -n root # Create logical root volume
 
 # Format partitions
+log_blue "Formatting the partitions..."
 mkfs.fat -F32 $EFI # EFI partition (FAT32)
 mkfs.ext4 /dev/mapper/luksvg-root -L root # Root partition (EXT4)
 mkswap /dev/mapper/luksvg-swap -L swap # Swap partition
 
 # Mount filesystems
+log_blue "Mounting filesystems..."
 mkdir /mnt/boot/efi # Create folder to hold EFI files
 mount $EFI /mnt/boot/efi # Mount EFI partition
 mount /dev/luksvg/root /mnt # Mount root partition
 swapon /dev/luksvg/swap # Activate swap partition
 
 # Install base packages
+log_blue "Bootstrapping Arch Linux into /mnt..."
 pacman -Syy
 pacstrap /mnt base base-devel git linux linux-firmware mkinitcpio sudo lvm2 dhcpcd wpa_supplicant nano zsh zsh-completions zsh-syntax-highlighting tilix $UCODE # Install base packages
 
 # fstab
+log_blue "Generating fstab file and setting 'noatime'..."
 genfstab -U /mnt > /mnt/etc/fstab # Generate fstab file
 sed -i 's/relatime/noatime/g' /mnt/etc/fstab # Replace 'relatime' with 'noatime' (Access time will not be saved in files)
-
-# Enter new system chroot
-arch-chroot /mnt # /mnt becomes temporary root directory
--- al_xen_part_b.sh
+++ al_xen_part_b.sh
@@ -1,10 +1,46 @@
+# Dependencies
+log_alert () {
+    echo "\e[1;33m[-] ALERT: $1\e[1;0m"
+}
+log_blue () {
+    echo "\e[1;34m$1\e[1;0m"
+}
+log_debug () {
+    echo "\e[1;36m[~] DEBUG: $1\e[1;0m"
+}
+log_error () {
+    echo "\e[1;31m[X] ERROR: $1\e[1;0m"
+}
+log_info () {
+    echo "\e[1;34m[*]\e[1;0m $1"
+}
+log_red () {
+    echo "\e[1;31m$1\e[1;0m"
+}
+log_success () {
+    echo "\e[1;32m[+] $1\e[1;0m"
+}
+log_yellow () {
+    echo "\e[1;33m$1\e[1;0m"
+}
+
+# *****************************************************************************************************************************************************
+# *****************************************************************************************************************************************************
+# *****************************************************************************************************************************************************
+
+
+# Enter new system chroot
+log_blue "Entering new system root and synchronizing pacman..."
+arch-chroot /mnt # /mnt becomes temporary root directory
 pacman -Syy # Synchronize Pacman repository again
 
 # Time
+log_blue "Setting timezone and hardware clock..."
 timedatectl set-timezone Europe/Berlin # Berlin timezone
 hwclock --systohc --utc # Assume hardware clock is UTC
 
 # Locale
+log_blue "Initializing the locale..."
 timedatectl set-ntp true # Enable NTP time synchronization again
 echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen # Change en-US (UTF-8) to en-GB (UTF-8)
 locale-gen # Generate locale
@@ -14,12 +50,14 @@ echo KEYMAP=de-latin1 > /etc/vconsole.conf # Set keyboard layout
 echo FONT=lat9w-16 >> /etc/vconsole.conf # Set console font
 
 # Network
+log_blue "Setting hostname and /etc/hosts..."
 echo Workstation > /etc/hostname # Set hostname
 echo "127.0.0.1 localhost" > /etc/hosts # Hosts file: Localhost (IP4)
 echo "::1 localhost" >> /etc/hosts # Hosts file: Localhost (IP6)
 echo "127.0.1.1 Workstation" >> /etc/hosts # Hosts file: This host (IP4)
 
 # initramfs
+log_blue "Rebuilding initramfs image using mkinitcpio..."
 echo "MODULES=()" > /etc/mkinitcpio.conf
 echo "BINARIES=()" >> /etc/mkinitcpio.conf
 #echo "FILES=()" >> /etc/mkinitcpio.conf
@@ -27,8 +65,11 @@ echo "HOOKS=(base udev autodetect modconf block filesystems keyboard fsck encryp
 mkinitcpio -p linux # Rebuild initramfs image
 
 # Users
+log_blue "Adding root and home user..."
+log_yellow "SET ROOT PASSWORD!"
 passwd # Set root password
 useradd -m -G wheel,users -s /usr/bin/zsh $USER # Add new user
+log_yellow "SET HOME USER PASSWORD!"
 passwd $USER # Set user password
 #echo "EDITOR=nano visudo" > /etc/sudoers # ???
 echo "root ALL=(ALL) ALL" > /etc/sudoers # Root account may execute any command
@@ -36,6 +77,7 @@ echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers # Users of group wheel may execute a
 echo "@includedir /etc/sudoers.d" >> /etc/sudoers # ???
 
 # Generate & integrate LUKS keyfile 
+log_blue "Adding keyfile for LUKS partition..."
 mkdir /root/keyfiles # Create folder to hold keyfiles
 chmod 700 /root/keyfiles # Protect keyfiles folder
 dd if=/dev/urandom of=/root/keyfiles/boot.keyfile bs=512 count=1 # Generate pseudorandom keyfile
@@ -46,6 +88,7 @@ echo "FILES=(/root/keyfiles/boot.keyfile)" >> /etc/mkinitcpio.conf # Adding keyf
 mkinitcpio -p linux # Recreate initramfs image
 
 # efibootmgr & GRUB
+log_blue "Installing GRUB with CRYPTODISK flag..."
 pacman -S --noconfirm efibootmgr grub # Install packages required for UEFI boot
 echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub # Enable booting from encrypted /boot
 sed -i 's/GRUB_CMDLINE_LINUX=""/#/g' /etc/default/grub # Disable default value
@@ -55,6 +98,7 @@ grub-mkconfig -o /boot/grub/grub.cfg # Generate GRUB configuration file
 chmod 700 /boot # Protect /boot
 
 # Install yay for access to the AUR ecosystem
+log_blue "Installing yay for access to the AUR ecosystem..."
 mkdir /home/$USER/Tools
 cd /home/$USER/Tools
 git clone https://aur.archlinux.org/yay.git
@@ -62,17 +106,20 @@ cd yay
 makepkg -si --noconfirm
 
 # Generate update script
+log_blue "Generating update script..."
 echo "sudo pacman -Syu --noconfirm" > /home/$USER/Tools/update.sh
 echo "yay -Syu --noconfirm" > /home/$USER/Tools/update.sh
 
 # Install required software
+log_blue "Installing required software..."
 pacman -S --noconfirm alsa-plugins alsa-utils gptfdisk keepass macchanger mousepad net-tools p7zip rkhunter thermald tlp unrar unzip wget zip #wget???
 yay -S --noconfirm chkrootkit secure-delete xen
 
 # Start services
+log_blue "Starting system services..."
 systemctl enable fstrim.timer # TRIM timer for SSDs
 systemctl enable systemd-timesyncd.service # Time synchronization
 
 # Finalization
+log_blue "Sync & Exit..."
 sync; exit # Synchronize and leave /mnt (temporary root directory)
-sync; reboot # Synchronize and reboot
